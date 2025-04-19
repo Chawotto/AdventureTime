d90c7238b22cd21c3e80bf822b7e1e1506f8ee5d
Esepenok Alexandr <145503317+Chawotto@users.noreply.github.com>
2025-04-19 13:23:27 +0300

    .

--- src/main/java/org/example/adventuretime/config/CorsConfig.java
+++ src/main/java/org/example/adventuretime/config/CorsConfig.java
@@ -8,14 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins(
-                        "https://adventuretime.up.railway.app",
-                        "https://adventuretimer-production-78c3.up.railway.app",
-                        "http://localhost:3000"
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
+        registry.addMapping("/**")
+                .allowedOrigins("adventuretime.up.railway.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
 }
