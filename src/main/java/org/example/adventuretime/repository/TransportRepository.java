b847027d0ef6a96355dcdc3d926ee2e21d4c0805
Esepenok Alexandr <145503317+Chawotto@users.noreply.github.com>
2025-04-20 12:01:50 +0300

    .

--- src/main/java/org/example/adventuretime/repository/TransportRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.adventuretime.repository;
-
-import org.example.adventuretime.model.Transport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransportRepository extends JpaRepository<Transport, Long> {
-}
